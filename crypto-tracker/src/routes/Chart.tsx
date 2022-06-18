97e9d8e29485009a23f4372e19b607d8854923b2
dennis903 <hyeongjin3@hanmail.net>
2022-06-18 18:47:16 +0900

    todo(crypto-tracker) : chart visualization

--- crypto-tracker/src/routes/Chart.tsx
+++ crypto-tracker/src/routes/Chart.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useQuery} from 'react-query';
 import { fetchCoinHistory } from '../api';
-import ApexChart from 'apexcharts';
+import ApexChart from 'react-apexcharts';
 
 interface IChartProps {
 	coinId?: string;
@@ -18,12 +18,52 @@ interface IHistorical {
 	volume: string;
 }
 
+interface ISeries {
+	name: string;
+	data: IHistorical;
+}
+
 function Chart({ coinId } : IChartProps) {
 	const {isLoading, data} = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
-	console.log(data);
+	const Series: ISeries[] =
+		[
+			{
+				name: 'Price',
+				data: data?.map(e => e.close)
+			}
+		]
 	return (
 		<div>
-			{isLoading ? "Loading chart..." : <ApexChart type="line" />}
+			{isLoading ? "Loading chart..." : <ApexChart 
+				type="line"
+				series={Series}
+				options={{
+					theme: {
+						mode: "dark",
+					},
+					chart: {
+						height: 300,
+						width: 500,
+						toolbar: {
+							show: false,
+						},
+						background: "transparent",
+					},
+					grid: { show: false },
+					stroke: {
+						curve: "smooth",
+						width: 4,
+					},
+					yaxis: {
+						show: false,
+					},
+					xaxis: {
+						axisBorder: { show: false },
+						axisTicks: { show: false },
+						labels: { show: false },
+					},
+				}}
+			/>}
 		</div>
 	)
 }
